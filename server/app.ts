fca35cdb7a4c5c4161f56a27e4bdb78c8a9e1a43
Insaf Inhaam <insafinhaam732@gmail.com>
2024-06-13 22:34:03 +0530

    server

--- server/app.ts
+++ server/app.ts
@@ -5,6 +5,8 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { ErrorMiddleware } from "./middleware/error";
 import userRouter from  './routes/user.route';
+import courseRouter from  './routes/course.route';
+import orderRouter from "./routes/order.route";
 
 //body parser
 app.use(express.json({ limit: "50mb" }));
@@ -21,6 +23,8 @@ app.use(
 
 //routes
 app.use('/api/v1', userRouter);
+app.use('/api/v1', courseRouter);
+app.use('/api/v1', orderRouter);
 
 // testing route
 app.get("/test", (_req: Request, res: Response, next: NextFunction) => {
---
+++ server/routes/course.route.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import { authorizeRoles, isAutheticated } from "../middleware/auth";
+import { addAnwser, addQuestion, addReplyToReview, addReview, editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../controller/course.controller";
+const courseRouter = express.Router();
+
+courseRouter.post(
+  "/create-course",
+  isAutheticated,
+  authorizeRoles("admin"),
+  uploadCourse
+);
+courseRouter.post(
+  "/edit-course/:id",
+  isAutheticated,
+  authorizeRoles("admin"),
+  editCourse
+);
+courseRouter.get("/get-course/:id", getSingleCourse);
+courseRouter.get("/get-courses", getAllCourses);
+courseRouter.get("/get-course-content/:id", isAutheticated, getCourseByUser);
+courseRouter.put("/add-question", isAutheticated, addQuestion);
+courseRouter.put("/add-answer", isAutheticated, addAnwser);
+courseRouter.put("/add-review/:id", isAutheticated, addReview);
+courseRouter.put("/add-reply/:id", isAutheticated, authorizeRoles("admin"), addReplyToReview);
+
+export default courseRouter;
